0f5f8c9d0b638eb02a7065dd4a5ff86ae8eab791
david kuseh <davegru203@gmail.com>
2021-06-30 19:37:52 +0000

    create chat UI, implement send message functionality

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from "@apollo/client";
+
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/',
@@ -15,19 +17,118 @@ query {
     }
 }`;
 
+const POST_MESSAGE = gql`
+mutation ($user:String!, $content:String!) {
+    postMessage(user: $user, content: $content)
+  }
+`
+
 const Messages = ({ user }) => {
-    const { data } = useQuery(GET_MESSAGES);
+    const { data } = useQuery(GET_MESSAGES, {
+        pollInterval: 500
+    });
 
     if(!data) {
         return null;
     }
 
-    return JSON.stringify(data);
+    return (
+        <>
+            {data.messages.map(({ id, user: messageUser, content }) => (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: user === messageUser ? "flex-end" : "flex-start",
+                        paddingBottom: "1em"
+                    }}
+                >
+                    {user !== messageUser && (
+                        <div
+                            style={{
+                                height: 50,
+                                width: 50, 
+                                marginRight: "0.5em",
+                                border: "2px solid #e5e6ea",
+                                borderRadius: 25,
+                                textAlign: "center",
+                                fontSize: "18pt", 
+                                paddingTop: 5
+                            }}
+                        >
+                            {messageUser.slice(0,2).toUpperCase()}
+                        </div>
+                    )}
+                    <div
+                        style={{
+                            background: user === messageUser ? "#58bf56" : "#e5e6ea",
+                            color: user === messageUser ? "white" : "black",
+                            padding: "1em",
+                            borderRadius: "1em",
+                            maxWidth: "60%"
+                        }}
+                    >
+                        {content}
+                    </div>
+                </div>
+            ))}
+        </>
+    )
 }
 
 const Chat = () => {
+    const [state, stateSet] = React.useState({
+        user: "Jack",
+        content:""
+    });
+    const [postMessage] = useMutation(POST_MESSAGE);
+
+    const onSend = () => {
+        if(state.content.length > 0){
+            postMessage({
+                variables: state
+            });
+        }
+        stateSet({
+            ...state,
+            content: ""
+        })
+    }
     return (
-        <div><Messages user='Jack' /></div>
+        <Container>
+            <Messages user={state.user} />
+            <Row>
+                <Col xs={2} style={{ padding: 0 }}>
+                    <FormInput 
+                        label="User"
+                        value={state.user}
+                        onChange={(evt) => stateSet({
+                            ...state,
+                            user: evt.target.value
+                        })}
+                    />
+                </Col>
+                <Col xs={8}>
+                    <FormInput 
+                        label="Content"
+                        value={state.content}
+                        onChange={(evt) => stateSet({
+                            ...state,
+                            content: evt.target.value
+                        })}
+                        onKeyUp={(evt) => {
+                            if(evt.keyCode === 13){
+                                onSend();
+                            }
+                        }}
+                    />
+                </Col>
+                <Col xs={2} style={{ padding: 0 }}>
+                    <Button onClick={() => onSend()} style={{ width: "100%" }}>
+                        Send
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
     )
 }; 
 
